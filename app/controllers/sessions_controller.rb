2a17c7c5aae79804b2479e1d99087eaa05af534a
Ian Kamau <kian99564@gmail.com>
2023-11-22 22:57:00 +0300

    Logout implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,22 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:user][:email])
-        if @user.present? && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to root_path, flash: { success: 'Logged in successfully' }
+            if @user.present? && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to root_path, flash: { success: 'Logged in successfully' }
         else
-        render :new
+            render :new
         end
     end
 
     def destroy
         session[:user_id] = nil
         redirect_to root_path, flash: { success: 'Logged Out' }
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :destroy]
-  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :sessions
   resources :posts
+
+  delete '/signout', to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
